22991354e65247d00bb64cb00193be376ecf0b4a
rockyweng75 <wengsc@yuntech.edu.tw>
2022-04-20 20:22:12 +0800

    first commit

---
+++ src/components/Html2Pdf/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="element-to-print">
+        <slot/>
+    </div>
+    <button v-on:click="print">print</button>
+</template>
+<script>
+import html2pdf from 'html2pdf.js'
+export default {
+    setup() {
+        const print = () =>{
+            var element = document.getElementById('element-to-print');
+            html2pdf(element).save();
+        }
+
+        return{
+            print
+        }
+    },
+}
+</script>
